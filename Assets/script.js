05e51ce6e09b5245a696bb59ff97e011d5a2c596
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-20 00:04:52 -0800

    placed loop function and fetch api function

--- Assets/script.js
+++ Assets/script.js
@@ -3,5 +3,16 @@
     
 // }, 1000);
 
+function initPage() {
 // my unique API so i can easily call back
 const APIKey = "eb11b651862feb056f1b8461d8892a0a"
+
+
+initPage();
+
+
+var APIKey = "&appid=eb11b651862feb056f1b8461d8892a0a=imperial"
+    //fetch api
+    fetch(
+            "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={eb11b651862feb056f1b8461d8892a0a}"
+            "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=eb11b651862feb056f1b8461d8892a0a=
